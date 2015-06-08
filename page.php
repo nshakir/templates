0bed3d087b18b3fee9a9460656f2d11d95f74c6d
nshakir <nectar.shakir@gmail.com>
2015-06-08 08:27:56 -0700

    due jun 5

--- page.php
+++ page.php
@@ -4,6 +4,10 @@
 <?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
 <h2><a href="<?php the_permalink(); ?>">
 <?php the_title(); ?></a></h2>
+<?php if (get_post_meta($post->ID, 'Quote', true)) : ?>
+<blockquote>Music expresses that which cannot be said and on which it is impossible to be silent.
+<br>Victor Hugo</br></blockquote>
+<?php endif; ?>
 <?php the_content(''); ?>
 <?php endwhile; endif; ?>
 <small>page.php</small>
